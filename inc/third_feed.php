853ea778c9269db08c1303270286369f0b767b63
pablo <pablo@commm.co>
2019-01-04 15:45:03 -0500

    grid img

--- inc/third_feed.php
+++ inc/third_feed.php
@@ -1,6 +1,6 @@
 <div class="card">
             <div class="card-image">
-              <img src="<?php the_post_thumbnail_url('comm-grid'); ?>">
+              <img src="<?php the_post_thumbnail_url(); ?>">
             </div>
 
             <div class="card-content grey-text text-darken-4" style="padding:8px;">
