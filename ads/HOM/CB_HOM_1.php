dec96f5f2cb93b2eaa4457b63f1c665536de20a2
Pablo <pablo@commm.co>
2019-03-08 16:46:18 -0500

    New ads comm

--- ads/HOM/CB_HOM_1.php
+++ ads/HOM/CB_HOM_1.php
@@ -6,7 +6,10 @@ googletag.cmd.push(function() { googletag.display('div-gpt-ad-1548543616200-0');
 </div>
 
 <div class="col s12 center" style="margin-top:10px;">
-        <div class="ads">
-            <a href="https://wplay.co/registro/casablanca" target="_blank"> <img src="http://casablancasports.com/wp-content/themes/commcbs/media/BANNER-WPLAY-CASA-BLANCA-ONCE-CALDAS-728x90.png"></a>
+        <div class="ads hide-on-small">
+            <a href="https://wplay.co/registro/casablanca" target="_blank"> <img src="<?php get_template_directory_uri() ?>/media/Banner-Casablanca-728x90.png"></a>
+        </div>
+        <div class="ads hide-on-med-and-up">
+            <a href="https://wplay.co/registro/casablanca" target="_blank"> <img src="<?php get_template_directory_uri() ?>/media/Banner-Casablanca-320x50"></a>
         </div>
     </div>
