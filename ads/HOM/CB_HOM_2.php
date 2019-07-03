9bc72750a8c170af2baecd782addf4e2f2cbf34c
Pablo <pablo@commm.co>
2019-07-03 10:01:32 -0500

    Fix link ad

--- ads/HOM/CB_HOM_2.php
+++ ads/HOM/CB_HOM_2.php
@@ -1,7 +1,7 @@
 
 
 <div class="ad col s12 center">
-    <a href="https://www.centrocomercialfundadores.com">
+    <a href="https://docs.google.com/forms/d/e/1FAIpQLSfsN064g4Pew_ktylw8z8sDQcx-SJ9bIb3W6Hzdnjes9rW2WA/viewform?fbclid=IwAR0luML9yCzBM11Sz5etEyocZ63SopsKY6zIlHhlxRbzEUmLzvuY9Phbh8c">
     <img src="<?php echo get_template_directory_uri() ?>/media/Middle-para-Caracol-III-Bootcamp-2019_300x250-px.gif">
     </a>
 </div>
