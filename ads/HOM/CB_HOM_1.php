08553e22451dea5e4a41a33b3381f79ad384075b
esteban <yean099@gmail.com>
2019-08-01 10:21:34 -0500

    corregido ids agregados a publicidades de casablanca para su seguimiento

--- ads/HOM/CB_HOM_1.php
+++ ads/HOM/CB_HOM_1.php
@@ -1,10 +1,10 @@
 
 
 <div class="col s12 center" style="margin-top:10px;">
-        <div id="p_b1" class="ads hide-on-small-only">
-            <a href="https://wplay.co/registro/casablanca" target="_blank"> <img src="<?php echo get_template_directory_uri() ?>/media/Banner-Casablanca-728x90.png"></a>
+        <div  class="ads hide-on-small-only">
+            <a href="https://wplay.co/registro/casablanca" target="_blank"> <img id="p_b1" src="<?php echo get_template_directory_uri() ?>/media/Banner-Casablanca-728x90.png"></a>
         </div>
         <div class="ads hide-on-med-and-up">
-            <a id="p_b1" href="https://wplay.co/registro/casablanca" target="_blank"> <img src="<?php echo get_template_directory_uri() ?>/media/Banner-Casablanca-320x50.png"></a>
+            <a  href="https://wplay.co/registro/casablanca" target="_blank"> <img id="p_b1" src="<?php echo get_template_directory_uri() ?>/media/Banner-Casablanca-320x50.png"></a>
         </div>
     </div>
