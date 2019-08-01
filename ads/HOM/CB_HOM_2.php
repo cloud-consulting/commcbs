f5199aa0b493267a642c96470109510826e6c82d
esteban <yean099@gmail.com>
2019-08-01 08:49:45 -0500

    Adicionando ids en publicidad para capturar clicks

--- ads/HOM/CB_HOM_2.php
+++ ads/HOM/CB_HOM_2.php
@@ -1,7 +1,7 @@
 
 
-<div class="ad col s12 center">
+<div id="p_a1" class="ad col s12 center">
     <a href="https://forms.gle/JKmHfwDDzp1yyetc9">
-    <img src="<?php echo get_template_directory_uri() ?>/media/expert-Volante-3_Redes-sociales-MAiling_Mesa-de-trabajo.jpg">
+    <img id="p_a2" src="<?php echo get_template_directory_uri() ?>/media/expert-Volante-3_Redes-sociales-MAiling_Mesa-de-trabajo.jpg">
     </a>
 </div>
