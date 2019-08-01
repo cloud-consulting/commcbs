6bb36b967a2f16236174801a09ffe2e4ddaf3dc7
esteban <yean099@gmail.com>
2019-08-01 10:16:38 -0500

    ids agregados a publicidades de casablanca para su seguimiento

--- ads/HOM/CB_HOM_3.php
+++ ads/HOM/CB_HOM_3.php
@@ -1,5 +1,5 @@
 
 
 <div class="ad col s12 center">
-<img src="http://casablancasports.com/wp-content/themes/commcbs/media/EMAS-BANNER-LIMPIEZA-300x250px.png">
+<img id="p_c1" src="http://casablancasports.com/wp-content/themes/commcbs/media/EMAS-BANNER-LIMPIEZA-300x250px.png">
 </div>
