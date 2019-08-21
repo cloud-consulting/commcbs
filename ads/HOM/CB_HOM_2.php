ad722ade9d9b26171170b7080f443e516bae66dc
esteban <yean099@gmail.com>
2019-08-21 13:06:33 -0500

    prueba reproducción automatica del video

--- ads/HOM/CB_HOM_2.php
+++ ads/HOM/CB_HOM_2.php
@@ -2,6 +2,6 @@
 
 <div class="ad col s12 center">
     <a href=" https://www.stm.com.co/portalstm/services/externos/stmcon/public/?utm_source=casablanca&utm_medium=web&utm_campaign=mora&utm_term=mora">
-    <video style="width: 50vw; height: 50vh;" id="p_a2" src="<?php echo get_template_directory_uri() ?>/media/gif_transito.mp4"></video>
+    <video style="width: 50vw; height: 50vh;" id="p_a2" src="<?php echo get_template_directory_uri() ?>/media/gif_transito.mp4" autoplay></video>
     </a>
 </div>
