15515c43b2d378eb3d944692b94ccac7c0dcef47
esteban <yean099@gmail.com>
2019-08-21 12:04:19 -0500

    actualizacion banner stm

--- ads/HOM/CB_HOM_2.php
+++ ads/HOM/CB_HOM_2.php
@@ -2,6 +2,6 @@
 
 <div class="ad col s12 center">
     <a href="https://forms.gle/JKmHfwDDzp1yyetc9">
-    <img id="p_a2" src="<?php echo get_template_directory_uri() ?>/media/expert-Volante-3_Redes-sociales-MAiling_Mesa-de-trabajo.jpg">
+    <video id="p_a2" src="<?php echo get_template_directory_uri() ?>/media/gif_transito.mp4"></video>
     </a>
 </div>
