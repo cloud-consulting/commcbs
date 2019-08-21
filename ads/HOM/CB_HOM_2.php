0402d895e3e30292cd56fb90831c18df21958cf4
esteban <yean099@gmail.com>
2019-08-21 12:14:57 -0500

    cambios de tamaño

--- ads/HOM/CB_HOM_2.php
+++ ads/HOM/CB_HOM_2.php
@@ -1,7 +1,7 @@
 
 
 <div class="ad col s12 center">
-    <a href="https://forms.gle/JKmHfwDDzp1yyetc9">
-    <video style="width: 10vw; height: 10vh;" id="p_a2" src="<?php echo get_template_directory_uri() ?>/media/gif_transito.mp4"></video>
+    <a href=" https://www.stm.com.co/portalstm/services/externos/stmcon/public/?utm_source=casablanca&utm_medium=web&utm_campaign=mora&utm_term=mora">
+    <video style="width: 50vw; height: 50vh;" id="p_a2" src="<?php echo get_template_directory_uri() ?>/media/gif_transito.mp4"></video>
     </a>
 </div>
