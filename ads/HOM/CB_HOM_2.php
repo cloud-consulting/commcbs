bf96c9174b3b5d2ed891d37c116d7a8bb7b74962
esteban <yean099@gmail.com>
2019-08-21 13:20:04 -0500

    pruebas con gif para banner stm

--- ads/HOM/CB_HOM_2.php
+++ ads/HOM/CB_HOM_2.php
@@ -1,7 +1,7 @@
 
 
 <div class="ad col s12 center">
-    <a href=" https://www.stm.com.co/portalstm/services/externos/stmcon/public/?utm_source=casablanca&utm_medium=web&utm_campaign=mora&utm_term=mora">
-    <video style="width: 50vw; height: 50vh;" id="p_a2" src="<?php echo get_template_directory_uri() ?>/media/gif_transito.mp4" autoplay></video>
+    <a href="https://www.stm.com.co/portalstm/services/externos/stmcon/public/?utm_source=casablanca&utm_medium=web&utm_campaign=mora&utm_term=mora">
+    <img id="p_a2" src="<?php echo get_template_directory_uri() ?>/media/gif_transito.gif">
     </a>
 </div>
