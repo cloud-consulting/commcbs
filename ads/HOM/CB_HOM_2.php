385f3d12c6b5037c25d86d7356f8d5bf8f88801b
esteban <yean099@gmail.com>
2019-08-21 12:10:21 -0500

    cambios de tamaño

--- ads/HOM/CB_HOM_2.php
+++ ads/HOM/CB_HOM_2.php
@@ -2,6 +2,6 @@
 
 <div class="ad col s12 center">
     <a href="https://forms.gle/JKmHfwDDzp1yyetc9">
-    <video id="p_a2" src="<?php echo get_template_directory_uri() ?>/media/gif_transito.mp4"></video>
+    <video style="width: 10vw; height: 10vh;" id="p_a2" src="<?php echo get_template_directory_uri() ?>/media/gif_transito.mp4"></video>
     </a>
 </div>
