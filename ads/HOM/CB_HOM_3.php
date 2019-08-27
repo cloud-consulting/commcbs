8ff2fa308d20365ec5dc7b016cecbfafe6dfa8db
esteban <yean099@gmail.com>
2019-08-27 15:03:32 -0500

    Camibios realizados a la URL de la campaña

--- ads/HOM/CB_HOM_3.php
+++ ads/HOM/CB_HOM_3.php
@@ -2,6 +2,6 @@
 
 <div class="ad col s12 center">
 	<a href="https://www.centrocomercialfundadores.com/campana-del-mes/">
-		<img id="p_c1" src="http://casablancasports.com/wp-content/themes/commcbs/media/ccfundadores.jpeg">
+		<img id="p_c1" src="<?php echo get_template_directory_uri() ?>/media/ccfundadores.jpeg">
 	</a>
 </div>
