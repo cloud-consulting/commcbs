80596d94db4e9673139e916ba3c239dd7c557f69
esteban <yean099@gmail.com>
2019-08-27 14:54:19 -0500

    Cambios realizados a banner publicitario

--- ads/HOM/CB_HOM_3.php
+++ ads/HOM/CB_HOM_3.php
@@ -1,5 +1,7 @@
 
 
 <div class="ad col s12 center">
-<img id="p_c1" src="http://casablancasports.com/wp-content/themes/commcbs/media/EMAS-BANNER-LIMPIEZA-300x250px.png">
+	<a href="https://www.centrocomercialfundadores.com/campana-del-mes/">
+		<img id="p_c1" src="http://casablancasports.com/wp-content/themes/commcbs/media/ccfundadores.jpeg">
+	</a>
 </div>
--- inc/sidebar.php
+++ inc/sidebar.php
@@ -1,42 +1,44 @@
 <!-- ADS 2ds -->
 <div style="margin-top:10px">
-<?php do_action('CB_HOM_2') ?>
+  <?php do_action('CB_HOM_2') ?>
 </div>
 
+
+<!-- ADS 3er -->
+<?php do_action('CB_HOM_3') ?>
 
 
 
 <div class="col s12 white">
 
-<ul class="collection no-border ">
+  <ul class="collection no-border ">
     <li class="collection-header black-text white">
-    <h4 class="flow-text">Te puede interesar</h4>
+      <h4 class="flow-text">Te puede interesar</h4>
 
     </li>
 
     <?php $args = array(
-  'numberposts' => '7',
-  'post_status' => 'publish',
-  'meta_key'		=> 'featured_sidebar',
-	 'meta_value'	=> '1'
-);
-      $recent_posts = wp_get_recent_posts($args);
-      foreach ($recent_posts as $recent) {
-        echo '
-        <li class="collection-item avatar">
-                <img src="' . esc_url(get_the_post_thumbnail_url($recent["ID"], 'comm-grid')) . '" class="circle square">
-                <span class="title">' . $recent['post_title']  .'</span><br>';
-        echo '<small class="grey-text">';
-        echo human_time_diff( get_the_time('U'), current_time('timestamp') ) . '&nbsp;';
-        echo '</small>';
-        echo ' <a class="view-more" href="' . get_permalink($recent["ID"]) . '"></a>';
-        echo '</li>';
+      'numberposts' => '7',
+      'post_status' => 'publish',
+      'meta_key'		=> 'featured_sidebar',
+      'meta_value'	=> '1'
+    );
+    $recent_posts = wp_get_recent_posts($args);
+    foreach ($recent_posts as $recent) {
+      echo '
+      <li class="collection-item avatar">
+      <img src="' . esc_url(get_the_post_thumbnail_url($recent["ID"], 'comm-grid')) . '" class="circle square">
+      <span class="title">' . $recent['post_title']  .'</span><br>';
+      echo '<small class="grey-text">';
+      echo human_time_diff( get_the_time('U'), current_time('timestamp') ) . '&nbsp;';
+      echo '</small>';
+      echo ' <a class="view-more" href="' . get_permalink($recent["ID"]) . '"></a>';
+      echo '</li>';
 
-      }
-      wp_reset_query();
-      ?>          
+    }
+    wp_reset_query();
+    ?>          
   </ul>     
-                </div>
+</div>
 
-<!-- ADS 3er -->
-                <?php do_action('CB_HOM_3') ?>
+
