a1812a0cbd59c9fef2205fbe9190bf2d2950b1bd
Carolina Castro <caro1853@gmail.com>
2019-09-18 18:21:16 -0500

    Change new bell

---
+++ ads/HOM/CB_HOM_5.php
@@ -0,0 +1,5 @@
+<div class="ad col s12 center">
+	<a href="https://wplay.co/registro/casablanca">
+		<img id="p_c5" src="<?php echo get_template_directory_uri() ?>/media/300x300.png">
+	</a>
+</div>
