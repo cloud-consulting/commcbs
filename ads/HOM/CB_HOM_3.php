8b572b922a6b101e0343bc85d1f1c2025173c3d2
Carolina Castro <caro1853@gmail.com>
2019-09-18 18:07:53 -0500

    change bell cc

--- ads/HOM/CB_HOM_3.php
+++ ads/HOM/CB_HOM_3.php
@@ -2,6 +2,6 @@
 
 <div class="ad col s12 center">
 	<a href="https://www.centrocomercialfundadores.com/">
-		<img id="p_c1" src="<?php echo get_template_directory_uri() ?>/media/campania-amor-y-amistad.jpg">
+		<img id="p_c1" src="<?php echo get_template_directory_uri() ?>/media/campana-amor-y-amistad.jpg">
 	</a>
 </div>
