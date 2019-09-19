d0bd01bada968bb2534156e9b1c885ffa58245cd
Carolina Castro <caro1853@gmail.com>
2019-09-19 09:18:50 -0500

    change banner small

--- ads/HOM/CB_HOM_1.php
+++ ads/HOM/CB_HOM_1.php
@@ -5,6 +5,6 @@
             <a href="https://wplay.co/registro/casablanca" target="_blank"> <img id="p_b1" src="<?php echo get_template_directory_uri() ?>/media/Banner-Casablanca-728x180.png"></a>
         </div>
         <div class="ads hide-on-med-and-up">
-            <a  href="https://wplay.co/registro/casablanca" target="_blank"> <img id="p_b1" src="<?php echo get_template_directory_uri() ?>/media/Banner-Casablanca-320x50.png"></a>
+            <a  href="https://wplay.co/registro/casablanca" target="_blank"> <img id="p_b1" src="<?php echo get_template_directory_uri() ?>/media/Banner-Casablanca-728x180.png"></a>
         </div>
     </div>
