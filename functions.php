175734034100a001b49099e76976d37e05bfcd3e
Gean <nullbyte16@gmail.com>
2019-09-26 14:14:58 -0500

    Adds static ad on posts
    Signed-off-by: Gean <nullbyte16@gmail.com>

--- functions.php
+++ functions.php
@@ -173,3 +173,40 @@ function theme_slug_widgets_init() {
 	'after_title'   => '</h2>',
     ));
 }
+?>
+
+<?php
+  
+//Insert ads after second paragraph of single post content.
+ 
+add_filter( 'the_content', 'prefix_insert_post_ads' );
+ 
+function prefix_insert_post_ads( $content ) {
+     
+    $ad_code = '<center><a href="https://www.instagram.com/victorjimenezye/?hl=es-la" target="_blank"><img style="height: 500px;width: auto;" src="http://casablancasports.com/wp-content/uploads/2019/09/e1fdc8db-ab3e-4ec2-bf86-5571095f9aa2.jpeg"></img></a><center>';
+ 
+    if ( is_single() && ! is_admin() ) {
+        return prefix_insert_after_paragraph( $ad_code, 2, $content );
+    }
+     
+    return $content;
+}
+  
+// Parent Function that makes the magic happen
+  
+function prefix_insert_after_paragraph( $insertion, $paragraph_id, $content ) {
+    $closing_p = '</p>';
+    $paragraphs = explode( $closing_p, $content );
+    foreach ($paragraphs as $index => $paragraph) {
+ 
+        if ( trim( $paragraph ) ) {
+            $paragraphs[$index] .= $closing_p;
+        }
+ 
+        if ( $paragraph_id == $index + 1 ) {
+            $paragraphs[$index] .= $insertion;
+        }
+    }
+     
+    return implode( '', $paragraphs );
+}
